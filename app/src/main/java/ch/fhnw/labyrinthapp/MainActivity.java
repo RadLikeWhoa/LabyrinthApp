8d7bd24c5ee1349f9914d7a971e65cd17f05484e
Sacha Schmid <schmid.sacha@gmail.com>
2016-05-18 16:49:39 +0200

    Improve MainActivity

--- app/src/main/java/ch/fhnw/labyrinthapp/MainActivity.java
+++ app/src/main/java/ch/fhnw/labyrinthapp/MainActivity.java
@@ -2,6 +2,7 @@ package ch.fhnw.labyrinthapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -9,49 +10,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 public class MainActivity extends Activity {
-    private Handler surfaceThreadHandler;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawingView(this));
-    }
-
-    class DrawingView extends SurfaceView implements SurfaceHolder.Callback {
-        public DrawingView(Context context) {
-            super(context);
-        }
-
-        @Override
-        public void surfaceCreated(SurfaceHolder surfaceHolder) {
-            Log.d("Surface", "created");
-
-            if (surfaceThreadHandler == null) {
-                SurfaceThread st = new SurfaceThread("surface", surfaceHolder);
-                st.start();
-
-                surfaceThreadHandler = st.getHandler();
-            }
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-            if (surfaceThreadHandler != null) {
-                Message msg = surfaceThreadHandler.obtainMessage();
-
-                msg.what = 1;
-
-                msg.sendToTarget();
-
-                surfaceThreadHandler = null;
-            }
-        }
+        setContentView(R.layout.activity_main);
     }
 }
